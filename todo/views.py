b4904fb58d36233daf2639fd2fa3c5ef1f4ad11f
npsytoh <npsytoh@gmail.com>
2024-08-19 22:27:16 +0900

    fix: Todo checkbox post proccess

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpRequest, HttpResponse
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from django.views import generic
 
 from .models import TodoItems
@@ -19,11 +22,6 @@ class TodoMainView(generic.ListView, generic.edit.ModelFormMixin):
         context["form_item"] = TodoCompletedModelForm(self.request.POST or None)
         return context
 
-    # def form_valid(self, form):
-    #     response = super().form_valid(form)
-    #     print(form.instance.pk)
-    #     return response
-
     def get(self, request, *args, **kwargs):
         self.object = None
         return super().get(request, *args, **kwargs)
@@ -37,18 +35,22 @@ class TodoMainView(generic.ListView, generic.edit.ModelFormMixin):
         else:
             return self.form_invalid(form)
 
-class TodoCheckToggleView(generic.UpdateView):
+class TodoCheckToggleView(generic.FormView):
     model = TodoItems
-    fields = ['is_completed']
+    form_class = TodoCompletedModelForm
     success_url = reverse_lazy('todo-main')
     http_method_names = ['post']
 
-    def form_valid(self, form):
-        print(self.request.POST)
-        post = form.save(commit=False)
-        post.is_completed = True
-        post.save()
-        return super().form_valid(form)
+    def post(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        obj = get_object_or_404(TodoItems, pk=pk) 
+        field_name = f'is_completed_{pk}'
+        if field_name in request.POST:
+            obj.is_completed = request.POST.get(field_name) == "on"
+        else:
+            obj.is_completed = False
+        obj.save()
+        return redirect(self.success_url)
 
 class TodoUpdateView(generic.UpdateView):
     model = TodoItems
