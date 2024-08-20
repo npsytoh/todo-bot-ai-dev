a4f95f11e01b685c0505e99c990faa07a38af24c
npsytoh <npsytoh@gmail.com>
2024-08-20 16:15:07 +0900

    refactor: Todo checkbox post script

--- static/js/project.js
+++ static/js/project.js
@@ -1,9 +1,32 @@
 const todoCheckboxes = document.querySelectorAll('input[type="checkbox"]');
 
+const changeClass = (element) => {
+	const todoTitle = document.getElementById(element.id.replace('checkbox', 'title'));
+	todoTitle.classList.toggle('text-decoration-line-through');
+};
+
+const postFormData = (form) => {
+	const formData = new FormData(form);
+	const action = form.getAttribute('action');
+	const options = {
+		method: 'POST',
+		body: formData,
+	};
+	fetch(action, options).then((e) => {
+		if (e.status === 200) {
+			console.log('[' + Date(Date.now()) + '] post success.');
+			return;
+		}
+		console.log('[' + Date(Date.now()) + '] post failed.');
+	});
+};
+
 todoCheckboxes.forEach((checkbox) => {
 	checkbox.addEventListener('change', () => {
 		const parentElement = checkbox.closest('span');
-		const todoTitle = document.getElementById(parentElement.id.replace('checkbox', 'title'));
-		todoTitle.classList.toggle('text-decoration-line-through');
+		changeClass(parentElement);
+
+		const parentForm = checkbox.closest('form');
+		postFormData(parentForm);
 	});
 });
