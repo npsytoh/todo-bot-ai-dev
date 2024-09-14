033bd6d7e0f30483439b49a9585c72956a7b7b1b
npsytoh <npsytoh@gmail.com>
2024-09-14 23:35:05 +0900

    refactor: todo/urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -3,12 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.TodoMainView.as_view(), name='todo-main'),
-    path('items/post/<slug:task_id>/<slug:target_field>/',
-         views.TodoItemPostView.as_view(), name='todo-item-post'),
-#     path('items/status-change/<slug:task_id>/',
-#          views.TodoStatusChangeView.as_view(), name='todo-status-change'),
-    path('items/edit/<slug:task_id>/',
-         views.TodoEditView.as_view(), name='todo-edit'),
-    path('items/delete/<slug:task_id>/',
-         views.TodoDeleteView.as_view(), name='todo-delete'),
+    path('items/post/<slug:task_id>/<slug:target_field>/', views.TodoItemPostView.as_view(), name='todo-item-post'),
+    path('items/edit/<slug:task_id>/', views.TodoEditView.as_view(), name='todo-edit'),
+    path('items/delete/<slug:task_id>/', views.TodoDeleteView.as_view(), name='todo-delete'),
 ]
